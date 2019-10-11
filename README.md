55e192c86e86b2a5a399e1f5b8929cd7b3426595
Jussi Nousiainen <juno.athome@gmail.com>
2019-10-11 14:57:27 +0200

    Completed the events lesson and challenge

--- README.md
+++ README.md
@@ -6,3 +6,28 @@ Interactive Frontend Development - From JavaScript to jQuery
 
 ### Items I covered on this course:
 
+1.  Using event triggers to change element properties on a webpage
+        -   .click()
+        -   .mouseover()
+        -   .mouseenter()
+        -   .mouseleave()
+
+2.  How to style elements without permanent changes by using addClass method
+        -   .addClass()
+        -   .removeClass()
+
+### Challenge
+
+Using addClass and removeClass:
+
+
+1.  changed the paragraph color globally on click
+
+2.  changed the h2 element color globally on hover
+
+3.  increased individual h2 elements font size while hovering over them
+
+4.  changed the page background color when hovering over a button
+
+5.  changed the page background color when moving off from over a button
+
--- script.js
+++ script.js
@@ -1,20 +1,99 @@
-$(document).ready(function() {
-    $("#stream1_btn").on("click", function() {
+$(document).ready(function () {
+    $("#stream1_btn").on("click", function () { //could be also written in form .click(function(){});
         $(".stream1").removeClass('highlight_stream');
         $(".stream2").removeClass('highlight_stream');
         $(".stream3").removeClass('highlight_stream');
         $(".stream1").addClass('highlight_stream');
-   });
-   $("#stream2_btn").on("click", function() {
+    });
+    $("#stream2_btn").on("click", function () {
         $(".stream1").removeClass('highlight_stream');
         $(".stream2").removeClass('highlight_stream');
         $(".stream3").removeClass('highlight_stream');
         $(".stream2").addClass('highlight_stream');
-   });
-   $("#stream3_btn").on("click", function() {
+    });
+    $("#stream3_btn").on("click", function () {
         $(".stream1").removeClass('highlight_stream');
         $(".stream2").removeClass('highlight_stream');
         $(".stream3").removeClass('highlight_stream');
         $(".stream3").addClass('highlight_stream');
-   });
-}); 
+    });
+    //1.     on click, change the color of all <p> to red
+    $("p").click(function () {
+        $("p").addClass("paragraph_color");
+    });
+
+    //2.    on hover, over <h2>, change the color of all <h2> to lightblue
+    $(".card_head").mouseover(function () {
+        $(".card_head").addClass("h2_hoverColor");
+    });
+
+    //3.    on hover, over <h2>, increase that <h2> font size to 1.4em
+    $("#head_html").mouseover(function () {
+        $("#head_html").addClass("h2_hoverFontSize");
+        $("#head_mysql").removeClass("h2_hoverFontSize");
+        $("#head_python").removeClass("h2_hoverFontSize");
+        $("#head_jquery").removeClass("h2_hoverFontSize");
+        $("#head_django").removeClass("h2_hoverFontSize");
+        $("#head_css").removeClass("h2_hoverFontSize");
+    });
+
+    $("#head_mysql").mouseover(function () {
+        $("#head_html").removeClass("h2_hoverFontSize");
+        $("#head_mysql").addClass("h2_hoverFontSize");
+        $("#head_python").removeClass("h2_hoverFontSize");
+        $("#head_jquery").removeClass("h2_hoverFontSize");
+        $("#head_django").removeClass("h2_hoverFontSize");
+        $("#head_css").removeClass("h2_hoverFontSize");
+    });
+
+    $("#head_python").mouseover(function () {
+        $("#head_html").removeClass("h2_hoverFontSize");
+        $("#head_mysql").removeClass("h2_hoverFontSize");
+        $("#head_python").addClass("h2_hoverFontSize");
+        $("#head_jquery").removeClass("h2_hoverFontSize");
+        $("#head_django").removeClass("h2_hoverFontSize");
+        $("#head_css").removeClass("h2_hoverFontSize");
+
+    });
+
+    $("#head_jquery").mouseover(function () {
+        $("#head_html").removeClass("h2_hoverFontSize");
+        $("#head_mysql").removeClass("h2_hoverFontSize");
+        $("#head_python").removeClass("h2_hoverFontSize");
+        $("#head_jquery").addClass("h2_hoverFontSize");
+        $("#head_django").removeClass("h2_hoverFontSize");
+        $("#head_css").removeClass("h2_hoverFontSize");
+
+    });
+
+    $("#head_django").mouseover(function () {
+        $("#head_html").removeClass("h2_hoverFontSize");
+        $("#head_mysql").removeClass("h2_hoverFontSize");
+        $("#head_python").removeClass("h2_hoverFontSize");
+        $("#head_jquery").removeClass("h2_hoverFontSize");
+        $("#head_django").addClass("h2_hoverFontSize");
+        $("#head_css").removeClass("h2_hoverFontSize");
+
+    });
+
+    $("#head_css").mouseover(function () {
+        $("#head_html").removeClass("h2_hoverFontSize");
+        $("#head_mysql").removeClass("h2_hoverFontSize");
+        $("#head_python").removeClass("h2_hoverFontSize");
+        $("#head_jquery").removeClass("h2_hoverFontSize");
+        $("#head_django").removeClass("h2_hoverFontSize");
+        $("#head_css").addClass("h2_hoverFontSize");
+    });
+
+    //4.    on hover on button, change the <body> background color to black
+    $(".bottom_button").mouseenter(function () {
+        $("body").addClass("page_toBlack");
+    });
+
+    //5.    on hover off button, change the <body> background color to grey
+    $(".bottom_button").mouseleave(function () {
+        $("body").addClass("page_toGrey");
+    });
+
+});
+
